d9f9b348d73b5c8118a788ae1d97cff81068bfa5
Dan Dickson <dandickson24@gmail.com>
2023-03-17 21:16:04 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const PI = 3.14
+
+// You can redeclare var, but you can not redeclare let. 
+// Var is function scope, where let is block scope.
+
+// var can be reassigned and redeclared, const can't do either of those things. 
+// Const is block scope and var is function scope
+
+// You can reassign let, but you can't reassign const.
+
+// Hoisting is when the browser reads the declarations first and then 
+// reads the values later on as it goes down through the document
